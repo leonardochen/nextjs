c3fa9442bf13a8f21739365595151416bfb99c4e
Leo Chen <leonardochen0@gmail.com>
2024-03-16 10:45:45 -0400

    Add Card

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -2,18 +2,40 @@
 
 import { Calendar } from "@/components/ui/calendar"
 import React, { useState } from 'react';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
     const [date, setDate] = useState<Date | undefined>(new Date())
 
     return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Calendar
-            mode="single"
-            selected={date}
-            onSelect={setDate}
-            className="rounded-md border"
-        />
-    </main>
-  );
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                className="rounded-md border"
+            />
+
+            <Card className="w-[350px] mt-[50px]">
+                <CardHeader>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
+
+        </main>
+    );
 }
